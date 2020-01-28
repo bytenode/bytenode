4efae791550da3a43ac49c69d6fdce73e747b06b
Osama Abbas <pw.osama@gmail.com>
2020-01-28 11:44:14 +0900

    an important correction

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ const spawnSync = require('child_process').spawnSync;
 const bytenode = require('./index.js');
 
 v8.setFlagsFromString('--no-lazy');
-v8.setFlagsFromString('--no-flush-bytecode'); // Thanks to Stephen Bradshaw (@greenimpala)
+v8.setFlagsFromString('--no-flush-bytecode'); // Thanks to A-Parser (@a-parser)
 
 let args = process.argv.slice(2);
 
