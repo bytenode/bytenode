0b798b90fc5282450fb6e0c3eda131e423b7b76b
Osama Abbas <pw.osama@gmail.com>
2020-06-08 16:36:22 +0900

    update electron version

--- examples/electron-hello-world/main-window.src.js
+++ examples/electron-hello-world/main-window.src.js
@@ -18,7 +18,7 @@ let mainWindow;
 
 function createWindow() {
 
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, webPreferences: { nodeIntegration: true } });
 
   mainWindow.loadFile('index.html');
 
