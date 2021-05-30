b7e8ab2d0602abb2eb366759e74aab9fe1e32512
Osama Abbas <pw.osama@gmail.com>
2021-05-31 06:14:30 +0900

    clearer test descriptions

--- test/test-index.js
+++ test/test-index.js
@@ -6,52 +6,54 @@ const assert = require('assert');
 const { it, section } = require('./runner.js');
 const Bytenode = require('../lib/index.js');
 
-section('Testing `Bytenode` API:');
+section('Testing Bytenode API:');
 
-it('compiles `code` to `bytecode`.', () => {
+it('.compile({ code }) returns a Buffer object.', () => {
   const code = 'console.log("it works");';
+
   const bytecodeBuffer = Bytenode.compile({ code });
+
   assert(Buffer.isBuffer(bytecodeBuffer));
 });
 
-it('compiles `filename.js` to `filename.jsc`.', () => {
+it('.compile({ filename }) compiles `filename` to `filename.jsc`.', () => {
   const filename = path.resolve(__dirname, './hapi-bundle.min.js');
-  Bytenode.compile({ filename });
+
+  const output = Bytenode.compile({ filename });
+
+  assert(output === filename.replace('.js', '.jsc'));
   assert(fs.existsSync(filename.replace('.js', '.jsc')));
+
   fs.unlinkSync(filename.replace('.js', '.jsc'));
 });
 
-it('compiles `filename.js` to `output` file.', () => {
+it('.compile({ filename, output }) compiles `filename` to `output`.', () => {
   const filename = path.resolve(__dirname, './hapi-bundle.min.js');
   const output = path.resolve(__dirname, './hapi-bundle.bin');
-  Bytenode.compile({ filename, output });
+
+  const returnedOutput = Bytenode.compile({ filename, output });
+
+  assert(output === returnedOutput);
   assert(fs.existsSync(output));
+
   // fs.unlinkSync(output); // keep it, it will be used in the next test.
 });
 
-it('registers ".bin" as bytecode file.', () => {
+it(".registerExtension('.bin') enable running '.bin' files.", () => {
   const output = path.resolve(__dirname, './hapi-bundle.bin');
+
   Bytenode.registerExtension('.bin');
+
   require(output);
+
   fs.unlinkSync(output);
 });
 
-it('compiles and runs `code` directly if `compileAsModule` is false', () => {
+it('.compile({ compileAsModule: false } produce non-module bytecode.', () => {
   const code = '42;';
   const compileAsModule = false;
+
   const bytecode = Bytenode.compile({ code, compileAsModule });
+
   assert(Bytenode.run({ bytecode }) === 42);
-});
-
-it('compiles and runs a complex, big javascript file', () => {
-  const filename = path.resolve(__dirname, './hapi-bundle.min.js');
-  const output = path.resolve(__dirname, './hapi-bundle.jsc');
-  Bytenode.compile({
-    filename,
-    output,
-    compileAsModule: true
-  });
-
-  require(output);
-  fs.unlinkSync(output);
 });
