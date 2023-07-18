66d1cc39e4f1c6148df303acc7f8edbbad87b6b2
Osama Abbas <pw.osama@gmail.com>
2023-07-18 15:50:48 +0900

    add `compress` param to index.d.ts file

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,21 +1,31 @@
 /// <reference types="node" />
-export declare const compileCode: (javascriptCode: string) => Buffer;
+export declare const compileCode: (javascriptCode: string, compress: boolean) => Buffer;
+
 interface ElectronCompileOptions {
     electronPath?: string;
 }
-export declare const compileElectronCode: (javascriptCode: string, options?: ElectronCompileOptions) => Promise<Buffer>;
+
+export declare const compileElectronCode: (javascriptCode: string, compress: boolean, options?: ElectronCompileOptions) => Promise<Buffer>;
+
 export declare const runBytecode: (bytecodeBuffer: Buffer) => any;
+
 interface BytenodeOptions {
     filename: string;
     output?: string;
     compileAsModule?: boolean;
+    compress?: boolean;
     electron?: boolean;
     electronPath?: string;
     createLoader?: boolean | string;
     loaderFilename?: string;
 }
+
 export declare const compileFile: (args: BytenodeOptions | string, output?: string | undefined) => Promise<string>;
+
 export declare const runBytecodeFile: (filename: string) => any;
+
 export declare function addLoaderFile(fileToLoad: string, loaderFilename?: string): void;
+
 export declare function loaderCode(targetPath: string): string;
+
 export { };
