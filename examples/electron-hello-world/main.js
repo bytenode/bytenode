488fab277efb75ccc53da1c45851eab9458f4127
Rafael F Beraldo <rafael.pimpa@gmail.com>
2024-04-23 14:34:10 -0300

    fix: update electron example

--- examples/electron-hello-world/main.js
+++ examples/electron-hello-world/main.js
@@ -7,8 +7,11 @@ const v8 = require('v8');
 v8.setFlagsFromString('--no-lazy');
 
 if (!fs.existsSync('./main-window.jsc')) {
-
-  bytenode.compileFile('./main-window.src.js', './main-window.jsc');
+  bytenode.compileFile({
+    filename: './main-window.src.js',
+    output: ''./main-window.jsc',
+    electron: true,
+  });
 }
 
 require('./main-window.jsc');
